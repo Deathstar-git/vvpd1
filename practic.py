6229f952c66708af2f6d737d05721484cdefcb98
Deathstar-git <79025632112tch@gmail.com>
2019-10-08 19:10:55 +0800

    working version with CLI

--- practic.py
+++ practic.py
@@ -4,28 +4,20 @@ import argparse
 
 def create_parser():
     p = argparse.ArgumentParser()
-    p.add_argument('count', choices=['Enter', 'enter'])
+    p.add_argument('x', type=int)
 
     return p
 
 
-def main():
-    x = input("Введите значение x:")
-    if x.isdigit():
-        x = int(x)
-        if math.cos(2 * int(x)) != 0 and ((2 * x) + math.sin(math.fabs(3 * int(x)))) / math.cos(2 * int(x)) > 0:
-            y = math.sqrt(((2 * x) + math.sin(math.fabs(3 * int(x)))) / math.cos(2 * int(x)))
-            print("Значение y равно:" + str(y))
-        else:
-            print("Не удалось посчитать выражение!")
-
-    try:
-        int(x)
-    except ValueError:
-        print("Введено неверное значение!")
+def main(x):
+    if math.cos(2 * int(x)) != 0 and ((2 * x) + math.sin(math.fabs(3 * int(x)))) / math.cos(2 * int(x)) > 0:
+        y = math.sqrt(((2 * x) + math.sin(math.fabs(3 * int(x)))) / math.cos(2 * int(x)))
+        print("Значение y равно:" + str(y))
+    else:
+        print("Не удалось посчитать выражение!")
 
 
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
-    main()
+    main(namespace.x)
